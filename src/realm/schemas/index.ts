3f2a913476047dfc7aace26c883e78e2eda7ad3e
timmyproofficial <timmyshow96@gmail.com>
2021-03-19 13:52:52 -0700

    Added index file containing all realm schema default exports

---
+++ src/realm/schemas/index.ts
@@ -0,0 +1,17 @@
+import CustomerSchema from './customers';
+import DebtManagerSchema from './debt_managers';
+import EmployeeSchema from './employees';
+import ExpenseSchema from './expenses';
+import ProductSchema from './products';
+import SaleSchema from './sales';
+import StockSchema from './stocks';
+
+export default {
+  CustomerSchema,
+  DebtManagerSchema,
+  EmployeeSchema,
+  ExpenseSchema,
+  ProductSchema,
+  SaleSchema,
+  StockSchema,
+};
