06d1dbe3331dbc2fc2348079732629adeaf8e186
timmyproofficial <timmyshow96@gmail.com>
2021-04-15 15:30:56 -0700

    removed validateFinishSale function for useMemo hook.

--- src/app/modules/ECommerce/pages/customersTransaction/CustomersCard.js
+++ src/app/modules/ECommerce/pages/customersTransaction/CustomersCard.js
@@ -1,45 +1,45 @@
-import React, { useState, useEffect, useMemo } from 'react';
-import * as Yup from 'yup';
+import React, { useState, useEffect, useMemo } from "react";
+import * as Yup from "yup";
 import {
   Card,
   CardBody,
   CardHeader,
-  CardHeaderToolbar,
-} from '../../../../../_metronic/_partials/controls';
-import { Formik } from 'formik';
-import { CustomersFilter } from './customers-filter/CustomersFilter';
-import { CustomersTable } from './customers-table/CustomersTable';
-import { CustomersGrouping } from './customers-grouping/CustomersGrouping';
-import { useCustomersUIContext } from './CustomersUIContext';
-import { useHistory } from 'react-router-dom';
-import helperFuns from '../utils/helper.funcs';
+  CardHeaderToolbar
+} from "../../../../../_metronic/_partials/controls";
+import { Formik } from "formik";
+import { CustomersFilter } from "./customers-filter/CustomersFilter";
+import { CustomersTable } from "./customers-table/CustomersTable";
+import { CustomersGrouping } from "./customers-grouping/CustomersGrouping";
+import { useCustomersUIContext } from "./CustomersUIContext";
+import { useHistory } from "react-router-dom";
+import helperFuns from "../utils/helper.funcs";
 
 export function CustomersCard(props) {
   const [disabled, setDisabled] = useState(true);
-  const [validateTransactionType, setValidateTransactionType] = useState('');
+  const [validateTransactionType, setValidateTransactionType] = useState("");
   const customersUIContext = useCustomersUIContext();
   const history = useHistory();
   const customersUIProps = useMemo(() => {
     return {
       ids: customersUIContext.ids,
       newCustomerButtonClick: customersUIContext.newCustomerButtonClick,
-      productsSelected: customersUIContext.productsSelected,
+      productsSelected: customersUIContext.productsSelected
     };
   }, [customersUIContext]);
 
-  const validateFinishSale = () => {
+  const validateFinishSale = useCallback(() => {
     const checkProduct = customersUIProps.productsSelected.some(
-      (item) => item.product
+      item => item.product
     );
     validateTransactionType && checkProduct && setDisabled(false);
-  };
+  });
 
   useEffect(() => {
     validateFinishSale();
   }, [
     customersUIProps.productsSelected,
     validateTransactionType,
-    validateFinishSale,
+    validateFinishSale
   ]);
 
   // const customerId = props.match.params.id;
@@ -47,10 +47,10 @@ export function CustomersCard(props) {
 
   const transactionTypeSchema = Yup.object().shape({
     transaction_type: Yup.string()
-      .min(2, 'Too Short!')
-      .max(50, 'Too Long!')
-      .required('Transaction type is required.'),
-    date: Yup.date().required('Date is required.'),
+      .min(2, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Transaction type is required."),
+    date: Yup.date().required("Date is required.")
   });
 
   return (
@@ -59,8 +59,8 @@ export function CustomersCard(props) {
         <CardHeaderToolbar>
           <Formik
             initialValues={{
-              transaction_type: '',
-              date: '',
+              transaction_type: "",
+              date: ""
             }}
             enableReinitialize={true}
             validationSchema={transactionTypeSchema}
@@ -71,7 +71,7 @@ export function CustomersCard(props) {
 
               let _newProductsSelected = [...customersUIProps.productsSelected];
               let grossTotal = 0;
-              _newProductsSelected.map((prod) => {
+              _newProductsSelected.map(prod => {
                 prod.productId = helperFuns.transformHexStringToObjectId(
                   prod.productId
                 );
@@ -83,14 +83,14 @@ export function CustomersCard(props) {
                 // customerId,
                 total_amount: grossTotal,
                 transaction_type: _transactionType,
-                date: _date,
+                date: _date
               };
 
               console.log(saveSale);
 
               // history.push('/e-commerce/sales');
 
-              resetForm({ values: '' });
+              resetForm({ values: "" });
             }}
           >
             {({
@@ -100,7 +100,7 @@ export function CustomersCard(props) {
               handleChange,
               setFieldValue,
               errors,
-              touched,
+              touched
             }) => (
               <form onSubmit={handleSubmit} className="form form-label-right">
                 <div className="form-group row">
@@ -110,8 +110,8 @@ export function CustomersCard(props) {
                       placeholder="Transaction type"
                       name="transaction_type"
                       onBlur={handleBlur}
-                      onChange={(e) => {
-                        setFieldValue('transaction_type', e.target.value);
+                      onChange={e => {
+                        setFieldValue("transaction_type", e.target.value);
                         setValidateTransactionType(e.target.value);
                         validateFinishSale();
                       }}
@@ -123,7 +123,7 @@ export function CustomersCard(props) {
                       <option value="card">Card</option>
                     </select>
                     {errors.transaction_type && touched.transaction_type ? (
-                      <div style={{ color: 'red' }}>
+                      <div style={{ color: "red" }}>
                         {errors.transaction_type}
                       </div>
                     ) : null}
@@ -139,12 +139,12 @@ export function CustomersCard(props) {
                       placeholder="Date"
                       onBlur={handleBlur}
                       value={values.date}
-                      onChange={(e) => {
-                        setFieldValue('date', e.target.value);
+                      onChange={e => {
+                        setFieldValue("date", e.target.value);
                       }}
                     />
                     {errors.date && touched.date ? (
-                      <div style={{ color: 'red' }}>{errors.date}</div>
+                      <div style={{ color: "red" }}>{errors.date}</div>
                     ) : null}
                     <small className="form-text text-muted">
                       <b>Date</b>
@@ -153,7 +153,7 @@ export function CustomersCard(props) {
                   <div className="col-lg-4">
                     <button
                       type="submit"
-                      style={{ display: 'block' }}
+                      style={{ display: "block" }}
                       className="btn btn-primary"
                       disabled={disabled}
                     >
