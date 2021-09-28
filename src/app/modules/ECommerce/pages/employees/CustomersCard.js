b3104e52ef00787cb9e3d0afd98d8b8138dba46a
timmyproofficial <timmyshow96@gmail.com>
2021-09-28 15:59:15 +0100

    Handled employees' filter component.'

--- src/app/modules/ECommerce/pages/employees/CustomersCard.js
+++ src/app/modules/ECommerce/pages/employees/CustomersCard.js
@@ -1,29 +1,29 @@
-import React, { useEffect, useMemo } from 'react';
-import { shallowEqual, useSelector, useDispatch } from 'react-redux';
+import React, { useEffect, useMemo } from "react";
+import { shallowEqual, useSelector, useDispatch } from "react-redux";
 import {
   Card,
   CardBody,
   CardHeader,
-  CardHeaderToolbar,
-} from '../../../../../_metronic/_partials/controls';
-import * as actions from '../../_redux/roles/rolesActions';
-import { CustomersFilter } from './customers-filter/CustomersFilter';
-import { EmployeesTable } from './customers-table/CustomersTable';
-import { EmployeesGrouping } from './customers-grouping/CustomersGrouping';
-import { useEmployeesUIContext } from './CustomersUIContext';
+  CardHeaderToolbar
+} from "../../../../../_metronic/_partials/controls";
+import * as actions from "../../_redux/roles/rolesActions";
+import { CustomersFilter } from "./customers-filter/CustomersFilter";
+import { EmployeesTable } from "./customers-table/CustomersTable";
+import { EmployeesGrouping } from "./customers-grouping/CustomersGrouping";
+import { useEmployeesUIContext } from "./CustomersUIContext";
 
 export function EmployeesCard() {
   const employeesUIContext = useEmployeesUIContext();
   const employeesUIProps = useMemo(() => {
     return {
       ids: employeesUIContext.ids,
-      newEmployeeButtonClick: employeesUIContext.newEmployeeButtonClick,
+      newEmployeeButtonClick: employeesUIContext.newEmployeeButtonClick
     };
   }, [employeesUIContext]);
 
   const { currentState } = useSelector(
-    (state) => ({
-      currentState: state.roles,
+    state => ({
+      currentState: state.roles
     }),
     shallowEqual
   );
